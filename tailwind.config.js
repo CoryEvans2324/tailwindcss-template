fea1784ded9cf87caf01fb0deb952507b3b67c75
Cory Evans <cory.evans@mailbox.org>
2021-06-28 17:28:40 +1200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	mode: "jit",
+	darkMode: "class",
+	purge: {
+		content: ["public/**/*.html", "public/**/*.js"],
+	},
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
